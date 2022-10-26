60c2ea173a930657b497b8553241737a1c12224d
Yasemin Kocak <yasemin.kocak@lapiscine.pro>
2022-10-26 22:29:18 +0200

    APP.JS + INDEX.JS Update

--- argentbank-front/src/index.js
+++ argentbank-front/src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./_store/_store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./styles/index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*
+Le store est ici un objet JavaScript qui va contenir les différentes
+informations ( de tous les composants) stockées globalement
+Il équivaut à State Global 
+(Magasin qui contient tout l'arbre d'état de l'application React)
+
+Le provider quant à lui va permettre à tous les 
+composants qui en ont besoin d'accéder au store
+*/
